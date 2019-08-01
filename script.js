ad4db3b6af63580c586b53d529c1f2bf4f1a550f
Roman <rombyr@outlook.com>
2019-08-01 22:49:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ var audio, context, analyser, src, array, logo;
 logo = document.getElementById("logo").style;
 
 audio = document.getElementById("audio");
+
+window.onclick = function(){
+    if(!context){
+        preparation();
+    }
+    if(audio.paused){
+        audio.play();
+        loop();
+    }else{
+        audio.pause();
+    }
+}
