3c4fbc92cc56746b17f736026c7b095702a600fc
Roman <rombyr@outlook.com>
2019-08-01 22:53:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ window.onclick = function(){
         audio.pause();
     }
 }
+
+function preparation(){
+    context = new AudioContext();
+    analyser = context.createAnalyser();
+    src = context.createMediaElementSource(audio);
+    src.connect(analyser);
+    analyser.connect(context.destination);
+    loop();
+}
+
+function loop(){
+    if(!audio.paused){
+        window.requestAnimationFrame(loop);
+    }
+    array = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(array);
+
+    logo.minHeight = (array[40])+"px";
+    logo.width =  (array[40])+"px";
+}
